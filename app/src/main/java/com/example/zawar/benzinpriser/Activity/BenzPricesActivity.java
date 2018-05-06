ed1418ddefe99b016978fc62829e09ba6891c5a6
Zawar Zul <zawarzul@gmail.com>
2018-05-06 21:48:41 +0200

    final version

--- app/src/main/java/com/example/zawar/benzinpriser/Activity/BenzPricesActivity.java
+++ app/src/main/java/com/example/zawar/benzinpriser/Activity/BenzPricesActivity.java
@@ -103,6 +103,7 @@ public class BenzPricesActivity extends AppCompatActivity implements GoogleApiCl
 
         Log.d(TAG, fuelType);
         //***************************//
+        //**************************//
 
         geoFire = new GeoFire(mGeoFire);
         Log.d(TAG, mDatabase.toString());
